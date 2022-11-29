50d4f367b081d53be2af9ceea7ccf3fc9c7234bc
Geshan Manandhar <geshan@simplywallst.com>
2022-11-29 22:26:45 +1100

    Adds tests for Controller and Service
    - [x] 100% code coverage

---
+++ test/unit/controllers/QuotesController.spec.ts
@@ -0,0 +1,40 @@
+import { QuotesController } from "../../../src/controllers/QuotesController";
+import { QuotesService } from '../../../src/services/QuotesService';
+
+describe('QuotesController', () => {
+  let controller: QuotesController;
+
+  const mockQuotesService = {
+    getQuotes: jest.fn()
+  } as QuotesService;
+  beforeEach(async () => {
+    controller = new QuotesController(mockQuotesService);
+    jest.clearAllMocks();
+  });
+
+  it('should define quotes controller', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getQuotes', () => {
+    it('should get quotes', () => {
+      mockQuotesService.getQuotes = jest.fn().mockReturnValueOnce([
+        {
+          id: 1,
+          quote: 'There are only two kinds of languages: the ones people complain about and the ones nobody uses.',
+          author: 'Bjarne Stroustrup'
+          },
+          {
+          id: 2,
+          quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+          author: 'Martin Fowler'
+          },
+      ]);
+
+      const quotes = controller.getQuotes();
+      expect(quotes).toHaveLength(2);
+      expect(quotes[0].author).toBe('Bjarne Stroustrup')
+      expect(quotes[1].quote).toEqual(expect.stringContaining('Any fool can write code that'));
+    });
+  });
+});
