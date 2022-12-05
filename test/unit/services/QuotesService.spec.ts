d94ec3731dc07bfb4394081fdfd10a8250c74141
Geshan Manandhar <geshan@simplywallst.com>
2022-12-05 15:56:11 +1100

    Better tests for the quotes service

--- test/unit/services/QuotesService.spec.ts
+++ test/unit/services/QuotesService.spec.ts
@@ -1,4 +1,5 @@
 import { QuotesService } from "../../../src/services/QuotesService";
+import { Quote } from "../../../src/types/Quote";
 
 describe('QuotesService', () => {
   let service: QuotesService;
@@ -12,12 +13,17 @@ describe('QuotesService', () => {
 
   describe('getQuotes', () => {
     it('should get mock fixed quotes', () => {
-      const quotes = service.getQuotes(1);
+      const quotes: Quote[] = service.getQuotes(1);
       expect(quotes).toHaveLength(10);
+      expect(quotes[0]).toEqual({
+        "author": "Bjarne Stroustrup",
+        "id": 1,
+        "quote": "There are only two kinds of languages: the ones people complain about and the ones nobody uses.",
+      });
     });
 
     it('should get mock fixed quotes for page > 1', () => {
-      const quotes = service.getQuotes(2);
+      const quotes: Quote[] = service.getQuotes(2);
       expect(quotes).toHaveLength(7);
     });
 
